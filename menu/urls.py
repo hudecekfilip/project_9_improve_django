e66005a51b2ab474eaf942e41cca762a7e800391
Filip Hudeček <filip.hudecek@icloud.com>
2018-07-18 14:29:43 +0200

    Minor changes

---
+++ menu/urls.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+from . import views
+
+app_name = 'menu'
+urlpatterns = [
+    path('', views.menu_view, name='index'),
+]
--- project_9_improve_django/settings.py
+++ project_9_improve_django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'menu',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'project_9_improve_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['menu/templates/menu'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
