ac56f92e446ad21181f6aca323fcd7b508519eca
Filip Hudeček <filip.hudecek@icloud.com>
2018-07-18 08:15:48 +0200

    Initial commit

---
+++ library/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/filiphudecek/Documents/python_web_development/projects/project_9_improve_django/library/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
