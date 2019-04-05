f9b39bed3823dae656847fd4baae5ab302a874e5
Filip Hudeček <filiphudecek@Filip-MacBook-Pro.local>
2019-04-05 13:14:02 +0200

    some changes

--- library/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/filiphudecek/Documents/python_web_development/projects/project_9_improve_django/library/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
